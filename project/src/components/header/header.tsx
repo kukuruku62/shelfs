de999b82e8d1fb371c1a25c1419beb22758ba198
Aleksei Falchuk <sddw28@gmail.com>
2024-11-28 10:05:56 +0100

    feat: add header

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,29 +1,37 @@
 "use server";
 import "server-only";
 
+import Image from "next/image";
 import Link from "next/link";
 import styles from "./styles.module.scss";
 
+import pictureFasade from "../../../public/img/svg/fasad.svg";
+
 
 const listLinks = [
-  { item: "1", name: "Domov", href: "/" },
-  { item: "2", name: "Skontrolovať objednávku", href: "/check-order" },
-  { item: "3", name: "Kontakt", href: "/contacts" },
+  { item: "1", img: pictureFasade, href: "/", title: "Domov" },
+  { item: "2", img: pictureFasade, href: "/contacts", title: "Kontakt" },
 ];
+
 
 export async function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <nav>
-          <ul className={styles.list}>
-            {listLinks.map(({ item, href, name }) => (
-              <li key={item}>
-                <Link href={href}>{name}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <div className={styles.content}>
+          <nav>
+            <ul className={styles.list}>
+              {listLinks.map(({ item, href, img, title }) => (
+                <li key={item}>
+                  <Link href={href} className={styles.link}>
+                    <p className={styles.nameLink}>{title}</p>
+                    <Image src={img} alt="" className={styles.pictureFasade} />
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </div>
       </div>
     </header>
   );
